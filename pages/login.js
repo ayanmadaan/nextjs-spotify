d3d3e9ad77a61344a9c7a964923f66725047609f
Ayan Madaan <madaanayan@gmail.com>
2022-01-17 23:26:56 +0530

    initial

---
+++ pages/login.js
@@ -0,0 +1,33 @@
+import { getProviders, signIn } from "next-auth/react";
+
+function Login({ providers }) {
+  return (
+    <div className="flex flex-col items-center bg-black min-h-screen w-full justify-center">
+      <h1>Login Page</h1>
+
+      {Object.values(providers).map((provider) => (
+        <div>
+          <button
+            className="bg-black text-slate-200 h-200 w-150 p-4 rounded-full"
+            onClick={() => {
+              signIn(provider.id, { callbackUrl: "/" });
+            }}
+          >
+            login with {provider.name}
+          </button>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default Login;
+
+export async function getServerSideProps() {
+  const providers = await getProviders();
+  return {
+    props: {
+      providers,
+    },
+  };
+}
